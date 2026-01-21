25b23e4ecad04205a562a2f4c9c4231a4099469f
Eliott Tadros <eliottt@kth.se>
2026-01-21 15:50:08 +0100

    feat #3: Added a testing framework (#23)
    I added a testing package that has an example test, using JUnit to run them

---
+++ src/dd2480/decide/tests/LicHelperTests.java
@@ -0,0 +1,12 @@
+package dd2480.decide.tests;
+
+import org.junit.jupiter.api.Test;
+
+import dd2480.decide.LicHelper;
+
+public class LicHelperTests {
+	@Test
+	void TestLicExample() {
+		LicHelper helper = new LicHelper();
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module dd2480.decide {
+	requires org.junit.jupiter.api;
 }
