fbaa8e199730b8d20f1edc4dae2b255ab2ba7849
Tim Bergbom <tbergbom@kth.se>
2026-01-22 08:36:13 +0100

    Add point class (#24)
    Co-authored-by: Tadros Eliott <eliottt@kth.se>

---
+++ src/main/java/Point.java
@@ -0,0 +1,12 @@
+/** 
+ * Simple 2-D point class
+ */
+public class Point {
+	public final double x; 
+	public final double y;
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+}
