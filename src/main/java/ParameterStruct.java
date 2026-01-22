f03524e6d0c294f07202165d9ae50483a7073e56
Felix Hansson Huber <43926314+seahoers@users.noreply.github.com>
2026-01-22 15:05:50 +0100

    feat #19: Add skeleton for calculateLIC method (#20)
    * Add ParamterStruct class
    
    * Add skeleton for calculateLIC method

---
+++ src/main/java/ParameterStruct.java
@@ -0,0 +1,64 @@
+
+public class ParameterStruct {
+    public final double LENGTH_1;
+    public final double LENGTH_2;
+    public final double RADIUS_1;
+    public final double RADIUS_2;
+    public final double EPSILON;
+    public final double AREA_1;
+    public final double AREA_2;
+    public final int QUADS;
+    public final double DIST;
+    public final int A_PTS;
+    public final int B_PTS;
+    public final int C_PTS;
+    public final int D_PTS;
+    public final int E_PTS;
+    public final int F_PTS;
+    public final int G_PTS;
+    public final int K_PTS;
+    public final int N_PTS;
+    public final int Q_PTS;
+
+    public ParameterStruct(
+        double length1,
+        double length2, 
+        double radius1,
+        double radius2,
+        double epsilon, 
+        double area1,
+        double area2,
+        int quads, 
+        double dist, 
+        int aPts, 
+        int bPts,
+        int cPts,
+        int dPts,
+        int ePts,
+        int fPts,
+        int gPts,
+        int kPts, 
+        int nPts,
+        int qPts
+    ) {
+        this.LENGTH_1 = length1;
+        this.LENGTH_2 = length2;
+        this.RADIUS_1 = radius1;
+        this.RADIUS_2 = radius2;
+        this.EPSILON = epsilon;
+        this.AREA_1 = area1;
+        this.AREA_2 = area2;
+        this.QUADS = quads;
+        this.DIST = dist;
+        this.A_PTS = aPts;
+        this.B_PTS = bPts;
+        this.C_PTS = cPts;
+        this.D_PTS = dPts;
+        this.E_PTS = ePts;
+        this.F_PTS = fPts;
+        this.G_PTS = gPts;
+        this.K_PTS = kPts;
+        this.N_PTS = nPts;
+        this.Q_PTS = qPts;
+    }
+}
--- src/test/java/LicHelperTests.java
+++ src/test/java/LicHelperTests.java
@@ -1,8 +1,17 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 
 public class LicHelperTests {
 	@Test
 	void TestLicExample() {
 		LicHelper helper = new LicHelper();
+	}
+
+	@Test
+	void calculateLIC_returnsFalse() {
+		boolean result = LicHelper.calculateLIC(null, 0, null, 0);
+
+		assertFalse(result);
 	}
 }
