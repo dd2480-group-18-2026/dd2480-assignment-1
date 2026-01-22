9e178859ccfde15dac6c3ae4897ee94cd5a6d47d
seahoers <f.hanssonhuber@gmail.com>
2026-01-22 12:17:54 +0100

    Add skeleton for calculateLIC method

--- src/main/java/LicHelper.java
+++ src/main/java/LicHelper.java
@@ -1,3 +1,39 @@
 public class LicHelper {
+    public static boolean calculateLIC(ParameterStruct parameters, int numPoints, Point[] points, int numOfLIC) {
+        switch (numOfLIC) {
+            case 0:
+                return false;
+            case 1:
+                return false;
+            case 2:
+                return false;
+            case 3:
+                return false;
+            case 4:
+                return false;
+            case 5:
+                return false;
+            case 6:
+                return false;
+            case 7:
+                return false;
+            case 8:
+                return false;
+            case 9:
+                return false;
+            case 10:
+                return false;
+            case 11:
+                return false;
+            case 12:
+                return false;
+            case 13:
+                return false;
+            case 14:
+                return false;
+            default:
+                return false;
+        }
+    }
 
 }
