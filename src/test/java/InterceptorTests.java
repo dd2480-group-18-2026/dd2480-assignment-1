12ece3421d0ee52de72271ee22272a41af15d9cc
seahoers <f.hanssonhuber@gmail.com>
2026-01-27 13:52:25 +0100

    feat #17: Rename InterceptorTest to InterceptorTests

--- src/test/java/InterceptorTests.java
+++ src/test/java/InterceptorTests.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class InterceptorTest {
+public class InterceptorTests {
     static final ParameterStruct PARAMETERS = new ParameterStruct(
         2, 
         0, 
