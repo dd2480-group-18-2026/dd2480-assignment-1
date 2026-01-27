c7f5b754b7fb9237c0b0da24b96ab4421b4922b7
seahoers <f.hanssonhuber@gmail.com>
2026-01-27 10:35:15 +0100

    feat #17: Refactor decide method

--- src/main/java/Interceptor.java
+++ src/main/java/Interceptor.java
@@ -4,16 +4,40 @@ public class Interceptor {
       boolean[][] pum = new boolean[15][15];
       boolean[] fuv = new boolean[15];
 
-      // Get Condition Met Vector (evaluate all LICs)
-      for (int i = 0; i < cmv.length; i++) {
-         cmv[i] = LicHelper.calculateLIC(parameters, numPoints, points, i);
-      }
+      populateCmv(cmv, parameters, numPoints, points);
+      
       
       for (int i = 0; i < cmv.length; i++) {
          System.out.println("CMV[" + i + "]: " + cmv[i]);
       }
 
-      // Get Preliminary Unlocking Matrix (use CMV and LCM)
+      populatePum(pum, cmv, lcm);
+
+      populateFuv(fuv, pum, puv);
+      
+
+      System.out.println("");
+      for (int i = 0; i < fuv.length; i++) {
+         System.out.println("FUV[" + i + "]: " + fuv[i]);
+      }
+
+      // Get launch decision (all FUV elements are true)
+      for (boolean licMet : fuv) {
+         if (!licMet) {
+            return "NO";
+         }
+      }
+
+      return "YES";
+   }
+
+   private static void populateCmv(boolean[] cmv, ParameterStruct parameters, int numPoints, Point[] points) {
+      for (int i = 0; i < cmv.length; i++) {
+         cmv[i] = LicHelper.calculateLIC(parameters, numPoints, points, i);
+      }
+   }
+
+   private static void populatePum(boolean[][] pum, boolean[] cmv, Connectors[][] lcm) {
       for (int i = 0; i < pum.length; i++) {
          for (int j = 0; j < pum[i].length; j++) {
             switch (lcm[i][j]) {
@@ -29,11 +53,9 @@ public class Interceptor {
             }
          }
       }
+   }
 
-      // System.out.println("LCM[1][2]: " + lcm[1][2]);
-      // System.out.println("PUM[1][2]: " + pum[1][2]);
-
-      // Get Final Unlocking Vector (use PUM and PUV)
+   private static void populateFuv(boolean[] fuv, boolean[][] pum, boolean[] puv) {
       for (int i = 0; i < pum.length; i++) {
          boolean fuvLicMet = true;
          if (puv[i]) {    
@@ -46,19 +68,5 @@ public class Interceptor {
          }
          fuv[i] = fuvLicMet;
       }
-
-      System.out.println("");
-      for (int i = 0; i < fuv.length; i++) {
-         System.out.println("FUV[" + i + "]: " + fuv[i]);
-      }
-
-      // Get launch decision (all FUV elements are true)
-      for (boolean licMet : fuv) {
-         if (!licMet) {
-            return "NO";
-         }
-      }
-
-      return "YES";
    }
 }
