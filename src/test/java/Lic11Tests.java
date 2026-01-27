d5749087b738584f586a41bbf292bae79a2846f2
tbengts <58944188+tbengts@users.noreply.github.com>
2026-01-27 07:33:28 +0100

    fix #13 Change doc-comments for tests
    Changed the doc comments for the tests to explicitly state the LIC11 condition.
    
    Changed ["consecutive"] to ["consecutive" intervening] in the comments for the G_PTS parameter.

--- src/test/java/Lic11Tests.java
+++ src/test/java/Lic11Tests.java
@@ -7,7 +7,7 @@ public class Lic11Tests {
 	
 	/**
 	 * This test asserts that the calculateLIC11 function correctly returns false
-	 * when the numPoints parameter is less than 3 (an invalid value).
+	 * when the numPoints parameter is less than 3 (invalid value).
 	 * The valid range for numPoints is 3 <= numPoints.
 	 */
 	@Test
@@ -28,7 +28,7 @@ public class Lic11Tests {
 			0, 
 			0, 
 			0, 
-			1, //Only one "consecutive" point
+			1, //Only one "consecutive" intervening point
 			0, 
 			0, 
 			0 
@@ -46,8 +46,12 @@ public class Lic11Tests {
 
 	/**
 	 * This test asserts that the calculateLIC11 function behaves correctly 
-	 * when given a valid set of parameters and points that do verify the
-	 * LIC 11 condition.
+	 * when given a valid set of parameters and points that do meet the
+	 * LIC 11 condition. The condition is met if there exists at least one
+	 * set of two data points, (X[i],Y[i]) and (X[j],Y[j]), separated by exactly
+	 * G_PTS consecutive intervening points, such that X[j] - X[i] < 0. 
+	 * (where i < j ) The condition is not met when NUMPOINTS < 3.
+	 * 1 ≤ G_PTS ≤ NUMPOINTS − 2
 	 */
     @Test
 	void calculateLIC11_returnsTrue_whenLIC11ConditionIsMet() {
@@ -67,7 +71,7 @@ public class Lic11Tests {
 			0, 
 			0, 
 			0, 
-			1, //Only one "consecutive" point
+			1, //Only one "consecutive" intervening point
 			0, 
 			0, 
 			0 
@@ -86,8 +90,12 @@ public class Lic11Tests {
     
 	/**
 	 * This test asserts that the calculateLIC11 function behaves correctly 
-	 * when given a valid set of parameters and points that do not verify 
-	 * the LIC 11 condition.
+	 * when given a valid set of parameters and points that do not meet 
+	 * the LIC 11 condition. The condition is met if there exists at least one
+	 * set of two data points, (X[i],Y[i]) and (X[j],Y[j]), separated by exactly
+	 * G_PTS consecutive intervening points, such that X[j] - X[i] < 0. 
+	 * (where i < j ) The condition is not met when NUMPOINTS < 3.
+	 * 1 ≤ G_PTS ≤ NUMPOINTS − 2
 	 */
     @Test
 	void calculateLIC11_returnsFalse_whenLIC11ConditionIsNotMet() {
@@ -107,7 +115,7 @@ public class Lic11Tests {
 			0, 
 			0, 
 			0, 
-			1, //Only one "consecutive" point
+			1, //Only one "consecutive" intervening point
 			0, 
 			0, 
 			0 
