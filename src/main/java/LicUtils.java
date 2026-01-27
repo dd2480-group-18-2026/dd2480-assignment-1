8dc5e26c50ad61e925a370dd17f25651a897ceb6
tbengts <58944188+tbengts@users.noreply.github.com>
2026-01-27 17:57:44 +0100

    feat #29: Add LicUtils class for all the LIC's helper methods (#35)
    Added an empty final class to be used for all of the helper methods
    
    fixes #29
    
    Co-authored-by: Tobias Bengtsson <tobiasbengtsson@MacBookAir.lan>

---
+++ src/main/java/LicUtils.java
@@ -0,0 +1,12 @@
+/**
+ * Contains all helper methods for all of the calculateLICX methods.
+ */
+public final class LicUtils {
+    private LicUtils() {
+        //No instatiation of this class. Only static helper methods.
+    }
+
+    //Helper methods:
+
+    //....
+}
