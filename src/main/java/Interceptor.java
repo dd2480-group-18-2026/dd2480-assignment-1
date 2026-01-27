37199108655ecf56fdec65e60beebddcbec63ff9
Tadros Eliott <53917558+Telmo26@users.noreply.github.com>
2026-01-27 12:33:32 +0100

    Added parallelism to the Interceptor class I also added a default case in 'main' taken from the tests to have a performance metric

--- src/main/java/Interceptor.java
+++ src/main/java/Interceptor.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Interceptor {
+   private static final int NUM_THREADS = Runtime.getRuntime().availableProcessors();
+   private static final ExecutorService THREAD_POOL = Executors.newFixedThreadPool(NUM_THREADS);
+   
    public static boolean decide(int numPoints, Point[] points, ParameterStruct parameters, Connectors[][] lcm, boolean[] puv) {
       if (!(lcm.length == 15 && lcm[0].length == 15)) {
          throw new IllegalArgumentException("The LCM has the wrong dimensions.");
@@ -12,9 +22,9 @@ public class Interceptor {
       boolean[][] pum = new boolean[15][15];
       boolean[] fuv = new boolean[15];
 
-      populateCmv(cmv, parameters, numPoints, points);
+      populateCmv(cmv, parameters, numPoints, points); 
       populatePum(pum, cmv, lcm);
-      populateFuv(fuv, pum, puv);
+      populateFuv(fuv, pum, puv);      
 
       // Get launch decision
       for (boolean licMet : fuv) {
@@ -27,41 +37,89 @@ public class Interceptor {
    }
 
    private static void populateCmv(boolean[] cmv, ParameterStruct parameters, int numPoints, Point[] points) {
+      List<Future<Void>> futures = new ArrayList<>(cmv.length);
+
       for (int i = 0; i < cmv.length; i++) {
-         cmv[i] = LicHelper.calculateLIC(parameters, numPoints, points, i);
+         final int index = i;
+         futures.add(THREAD_POOL.submit(() -> {
+            cmv[index] = LicHelper.calculateLIC(parameters, numPoints, points, index);
+            return null;
+         }));
+      }
+
+      for (Future<Void> future : futures) {
+         try {
+            future.get();
+         } catch (InterruptedException e) {
+               System.err.println("Thread interrupted: " + e);
+         } catch (ExecutionException e) {
+               System.err.println("Computation threw exception: " + e.getCause());
+         }
       }
    }
 
    private static void populatePum(boolean[][] pum, boolean[] cmv, Connectors[][] lcm) {
+      List<Future<Void>> futures = new ArrayList<>(pum.length);
+
       for (int i = 0; i < pum.length; i++) {
-         for (int j = 0; j < pum[i].length; j++) {
-            switch (lcm[i][j]) {
-               case Connectors.ANDD:
-                  pum[i][j] = cmv[i] && cmv[j];
-                  break;
-               case Connectors.ORR:
-                  pum[i][j] = cmv[i] || cmv[j];
-                  break;
-               case Connectors.NOTUSED:
-                  pum[i][j] = true;
-                  break;
+         final int index = i;
+         futures.add(THREAD_POOL.submit(() -> {
+            for (int j = 0; j < pum[index].length; j++) {
+               switch (lcm[index][j]) {
+                  case Connectors.ANDD:
+                     pum[index][j] = cmv[index] && cmv[j];
+                     break;
+                  case Connectors.ORR:
+                     pum[index][j] = cmv[index] || cmv[j];
+                     break;
+                  case Connectors.NOTUSED:
+                     pum[index][j] = true;
+                     break;
+               }
             }
+            return null;
+         }));
+      }
+
+      for (Future<Void> future : futures) {
+         try {
+            future.get();
+         } catch (InterruptedException e) {
+               System.err.println("Thread interrupted: " + e);
+         } catch (ExecutionException e) {
+               System.err.println("Computation threw exception: " + e.getCause());
          }
       }
    }
 
    private static void populateFuv(boolean[] fuv, boolean[][] pum, boolean[] puv) {
+      List<Future<Void>> futures = new ArrayList<>(pum.length);
+
       for (int i = 0; i < pum.length; i++) {
-         boolean fuvLicMet = true;
-         if (puv[i]) {    
-            for (int j = 0; j < pum[i].length; j++) {
-               if (!pum[i][j]) {
-                  fuvLicMet = false;
-                  break;
+         final int index = i;
+         futures.add(THREAD_POOL.submit(() -> {
+            boolean fuvLicMet = true;
+            if (puv[index]) {    
+               for (int j = 0; j < pum[index].length; j++) {
+                  if (!pum[index][j]) {
+                     fuvLicMet = false;
+                     break;
+                  }
                }
             }
+            fuv[index] = fuvLicMet;
+            return null;
+         }));        
+      }
+
+      for (Future<Void> future : futures) {
+         try {
+            future.get();
+         } catch (InterruptedException e) {
+               System.err.println("Thread interrupted: " + e);
+         } catch (ExecutionException e) {
+               System.err.println("Computation threw exception: " + e.getCause());
          }
-         fuv[i] = fuvLicMet;
       }
    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,107 @@
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-	}
+		ParameterStruct PARAMETERS = new ParameterStruct(
+			2, 
+			0, 
+			0, 
+			0, 
+			1e-6, 
+			1, 
+			10, 
+			2, 
+			0, 
+			1, 
+			1, 
+			1, 
+			1, 
+			1, 
+			1, 
+			1, 
+			1, 
+			1, 
+			3
+		);
 
+		Connectors[][] LCM = {
+			{Connectors.ANDD, Connectors.ORR, Connectors.ORR, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ANDD, Connectors.ANDD, Connectors.ORR, Connectors.ORR, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ORR, Connectors.ORR, Connectors.ANDD, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ANDD, Connectors.ORR, Connectors.ORR, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ANDD, Connectors.ORR, Connectors.ORR, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ANDD, Connectors.ORR, Connectors.ORR, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ANDD, Connectors.ANDD, Connectors.ORR, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ANDD, Connectors.ANDD, Connectors.ORR, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ANDD, Connectors.ANDD, Connectors.ORR, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ANDD, Connectors.ORR, Connectors.ORR, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ANDD, Connectors.ORR, Connectors.ORR, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ANDD, Connectors.ORR, Connectors.ORR, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.ANDD, Connectors.ANDD, Connectors.ORR, Connectors.ANDD, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED},
+			{Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.NOTUSED, Connectors.ANDD},
+		};
+
+		boolean[] PUV = {true, false, false, true, true, true, false, false, false, true, true, true, true, false, true};
+
+		Point[] points = {
+            new Point(-2, 3),
+            new Point(0, 0),
+            new Point(1, -1),
+            new Point(1, 2),
+            new Point(3, 3),
+            new Point(3, 4),
+            new Point(5, 4),
+            new Point(11, 10),
+            new Point(13, 12),
+            new Point(10, 17),
+			new Point(-2, 3),
+            new Point(0, 0),
+            new Point(1, -1),
+            new Point(1, 2),
+            new Point(3, 3),
+            new Point(3, 4),
+            new Point(5, 4),
+            new Point(11, 10),
+            new Point(13, 12),
+            new Point(10, 17),
+			new Point(-2, 3),
+            new Point(0, 0),
+            new Point(1, -1),
+            new Point(1, 2),
+            new Point(3, 3),
+            new Point(3, 4),
+            new Point(5, 4),
+            new Point(11, 10),
+            new Point(13, 12),
+            new Point(10, 17),
+			new Point(-2, 3),
+            new Point(0, 0),
+            new Point(1, -1),
+            new Point(1, 2),
+            new Point(3, 3),
+            new Point(3, 4),
+            new Point(5, 4),
+            new Point(11, 10),
+            new Point(13, 12),
+            new Point(10, 17),
+			new Point(-2, 3),
+            new Point(0, 0),
+            new Point(1, -1),
+            new Point(1, 2),
+            new Point(3, 3),
+            new Point(3, 4),
+            new Point(5, 4),
+            new Point(11, 10),
+            new Point(13, 12),
+            new Point(10, 17),
+        };
+
+		boolean result = Interceptor.decide(points.length, points, PARAMETERS, LCM, PUV);
+
+		if (result) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+		System.exit(0);
+	}
 }
