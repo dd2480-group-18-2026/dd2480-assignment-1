a95a12e498c4b9cb28ace919b8883231779868f2
tbengts <58944188+tbengts@users.noreply.github.com>
2026-01-28 18:43:48 +0100

    refactor #46: refactored all calculateLIC methods (#49)
    - Refactored calculateLIC methods so that they use helper methods in LicUtils
    - Removed methods angle() and distanceTo() in the point class
    - Removed tests of the angle method in the point class
    
    Co-authored-by: Tobias Bengtsson <tobiasbengtsson@MacBookAir.lan>

--- src/main/java/LicUtils.java
+++ src/main/java/LicUtils.java
@@ -25,4 +25,35 @@ public final class LicUtils {
         double dy = a.y - b.y;
         return Math.sqrt(dx * dx + dy * dy);
     }
+
+	static double angle(Point A, Point B, Point C) {
+		double BA_angle = Math.atan2(A.y - B.y, A.x - B.x);
+		double BC_angle = Math.atan2(C.y - B.y, C.x - B.x);
+
+		double angle = BA_angle - BC_angle;
+		return angle;
+    }
+
+    static double calculateTriangleArea(Point A, Point B, Point C) {
+        double area = Math.abs(
+                (A.x * (B.y - C.y) +
+                 B.x * (C.y - A.y) +
+                 C.x * (A.y - B.y)) / 2.0
+            );
+        return area;
+    }
+
+    static double calculateRequiredRadius(double area, double longest, double ab, double bc, double ca) {
+        double requiredRadius;
+        if (area == 0.0) {
+                requiredRadius = longest / 2.0;
+            } 
+        else {
+            double rCircumradius = (ab * bc * ca) / (4.0 * area);
+            double rLongestSide = longest / 2.0;
+            requiredRadius = Math.max(rLongestSide, rCircumradius);
+        }
+        return requiredRadius;
+    }
+
 }
