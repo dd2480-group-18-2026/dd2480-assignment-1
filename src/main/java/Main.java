5a89ae2e20e193dbdc4f4a745fbd430092dc620f
Eliott Tadros <53917558+Telmo26@users.noreply.github.com>
2026-01-28 18:13:23 +0100

    feat. #34: Implemented JSON parsing (#52)
    * feat. #34: Implemented JSON parsing for inputs
    This is done using the Jackson library, so modifications to the pom.xml were needed. I also needed to adapt the Point constructor
    
    * Converted all the 'decide' tests into 'main' tests
    I moved the configuration of each test into their own test JSON files in src/test/resources

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,23 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		if (args.length < 1) {
+			System.out.println("No problem file specified, aborting...");
+			System.exit(1);
+		}
+
+		String jsonPath = args[0];
+		
+		boolean launch = run(jsonPath);
+
+		System.out.println(launch ? "YES" : "NO");
+
+		System.exit(0);
 	}
 
+	public static boolean run(String jsonPath) {
+		InputParser inputParser = new InputParser(jsonPath);
+
+		return Interceptor.decide(inputParser.points.length, inputParser.points, inputParser.parameters, inputParser.LCM, inputParser.PUV);
+	}
 }
--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /** 
  * Simple 2-D point class
  */
@@ -5,10 +8,14 @@ public class Point {
 	public final double x; 
 	public final double y;
 
-	public Point(double x, double y) {
-		this.x = x;
-		this.y = y;
-	}
+	@JsonCreator
+    public Point(
+        @JsonProperty("x") double x,
+        @JsonProperty("y") double y
+    ) {
+        this.x = x;
+        this.y = y;
+    }
 
 	@Override
 	public boolean equals(Object o) {
